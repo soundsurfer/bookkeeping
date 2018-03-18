f4e5f5cf0ee1413e8f22dafd49b9be95f8a51779
Valentin, the Creator <valik7444@ukr.net>
2018-03-18 19:46:02 +0200

    added bookkeeping class for program implementation

---
+++ src/main/java/com/bookkeeping/budget/Budget.java
@@ -0,0 +1,37 @@
+package com.bookkeeping.budget;
+
+import com.bookkeeping.com.bookkeeping.db.DataBaseManager;
+
+import java.util.Scanner;
+
+public class Budget {
+    static Scanner scanner = new Scanner(System.in);
+    private static int m;
+    private static int s;
+    private static String p;
+    public static void budgetRun(String name){
+        addMoney();
+        spentMoney();
+        nameProduct();
+        createBudget(name, m, s, p);
+    }
+    private static void addMoney(){
+        System.out.println("Please, enter how much money you add to budget:");
+        m = scanner.nextInt();
+    }
+    private static void spentMoney(){
+        System.out.println("How much money you spent today?");
+        s = scanner.nextInt();
+    }
+    private static void nameProduct(){
+        System.out.println("What did you spend money on?");
+        p = scanner.nextLine();
+    }
+    public static void budgetStatus(){
+        System.out.println("Now you have:" + m);
+        System.out.println("You spent today:" + s);
+    }
+    private static void createBudget(String name, int m, int s, String p){
+        DataBaseManager.dataBaseCreate(name, m, s, p);
+    }
+}
