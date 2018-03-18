d0f7d0ac6d61feb51123e66501bef0e49f07fc3d
Yaroslav <yarbezl@gmail.com>
2018-03-18 23:03:24 +0200

    Update 0.0.1

---
+++ src/main/java/com/bookkeeping/db/DataBaseManager.java
@@ -0,0 +1,64 @@
+package com.bookkeeping.db;
+
+import com.bookkeeping.utility.Settings;
+
+import java.sql.*;
+
+public class DataBaseManager {
+    private static Connection connection = null;
+
+    private static Statement statement;
+
+    private static Connection getConnection() {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found , try again , later ...");
+            //e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(
+                    Settings.properties.getProperty("URL"),
+                    Settings.properties.getProperty("USER"),
+                    Settings.properties.getProperty("PASSWORD"));
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            //e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void insert(String name, int m, int s, String p) {
+        try {
+            statement = DataBaseManager.getConnection().createStatement();
+            statement.execute(String.format("INSERT INTO BOOKKEEPING (name, budget, spentmoney, nameproduct) " +
+                    "VALUES ('%s',%d,%d,'%s')", name, m, s, p));
+        } catch (SQLException e) {
+            System.out.println("Can't create statement!!!");
+            e.printStackTrace();
+        }
+    }
+
+    public static ResultSet getByName(String name) {
+        try {
+            statement = DataBaseManager.getConnection().createStatement();
+            return statement.executeQuery(String.format("SELECT * FROM BOOKKEEPING WHERE name = '%s'", name));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ResultSet getById(int id) {
+        try {
+            Statement statement = DataBaseManager.getConnection().createStatement();
+            return statement.executeQuery(String.format("SELECT * FROM BOOKKEEPING WHERE id = '%d'", id));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
+
