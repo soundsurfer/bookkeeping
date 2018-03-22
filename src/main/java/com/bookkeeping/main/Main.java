aeb5064424abcdfc26eff605d4a7d7d587bd5624
Valentin, the Creator <valik7444@ukr.net>
2018-03-22 20:30:31 +0200

    update main class.

--- src/main/java/com/bookkeeping/main/Main.java
+++ src/main/java/com/bookkeeping/main/Main.java
@@ -2,6 +2,7 @@ package com.bookkeeping.main;
 
 import com.bookkeeping.budget.Budget;
 import com.bookkeeping.db.DataBaseManager;
+import com.bookkeeping.utility.Menu;
 import com.bookkeeping.utility.Settings;
 
 import java.util.Scanner;
@@ -9,22 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Settings.readConfig();
-        start();
-    }
-
-    private static void start() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hi! My name is Bookkeeping Manager, what's your name?");
-        String name = scanner.next();
-        System.out.println("Okay, " + name + ", let's go...");
-        Budget.budgetRun(name);
-        System.out.println("Hey, you needing some info about your budget? Print 'y' or 'n'");
-        if (scanner.hasNext("y")) {
-            Budget.budgetStatus();
-        }
-        if (scanner.hasNext("n")) {
-            System.out.println("Okay, see you later ;)");
-        } else ;
+        Menu.menu1();
 
     }
 }
