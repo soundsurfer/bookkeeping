f6b6154098034dbb70fcea0c46c917cebb3de3b0
Valentin, the Creator <valik7444@ukr.net>
2018-03-22 20:29:48 +0200

    created methods for menu class.

--- src/main/java/com/bookkeeping/utility/Menu.java
+++ src/main/java/com/bookkeeping/utility/Menu.java
@@ -1,5 +1,38 @@
 package com.bookkeeping.utility;
 
-public class Menu {
+import com.bookkeeping.budget.Budget;
+import com.bookkeeping.db.DataBaseManager;
 
+import java.util.Scanner;
+
+public class Menu {
+    static Scanner scanner = new Scanner(System.in);
+    public static void menu1() {
+        System.out.println("Hi! My name is Bookkeeping Manager, what's your name?");
+        String name = scanner.next();
+        System.out.println("Hi! " + name + ", what you need?");
+        if (scanner.hasNext("newbudget")) {
+            Budget.budgetRun(name);
+            taskMenu1();
+        }
+        if (scanner.hasNext("budgetstatus")) {
+            System.out.println("Please, enter your name for viewing your budget:");
+            scanner.hasNext(name);
+            DataBaseManager.getByBudget(name);
+        }
+    }
+    public static void taskMenu1(){
+        System.out.println("Hey, you needing some info about your budget? Print 'y' or 'n'");
+        if (scanner.hasNext("y")) {
+            Budget.budgetStatus();
+        }
+        if (scanner.hasNext("n")) {
+            System.out.println("Okay, you wanna back?");
+            if (scanner.hasNext("y")){
+                menu1();
+            }
+            else ;
+        } else ;
+    }
 }
+
